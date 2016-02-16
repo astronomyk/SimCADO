e1f8eb3f9ebaa384d8ef1cbecf69815e4fcb3160
oczoske <ollicz68@yahoo.com>
2016-02-16 18:39:23 +0100

    Preliminary adaptation to module SpectralCurve, cosmetics.

--- SimCADO/SimCADO/AtmosphereModel.py
+++ SimCADO/SimCADO/AtmosphereModel.py
@@ -1,15 +1,16 @@
+"""Atmosphere model"""
 ###############################################################################
 # AtmosphereModel
 #
 # DESCRIPTION
 # AtmosphereModel holds the transmission and emission curves as well as info
-# on the environment variables: Temp, Pres, Rel Hum, z0. It should be able to 
+# on the environment variables: Temp, Pres, Rel Hum, z0. It should be able to
 # decide whether a skycalc file is being read, of a series of 2 ascii files.
-# 
-# This script also contains a series of helpful functions. 
-# - atmospheric_refraction() computes the angular difference between the 
+#
+# This script also contains a series of helpful functions.
+# - atmospheric_refraction() computes the angular difference between the
 #     apparent position of a star seen from the ground and its true position.
-# 
+#
 # Classes:
 #   AtmosphereModel(object)
 #   - __init__(UserCommands)
@@ -26,9 +27,10 @@
 #
 
 
+import numpy as np
 from astropy import units as u
 from astropy.io import fits
-from throughput import *
+import SimCADO.SpectralCurve as sc
 
 ## These classes and functions are exported to the package
 __all__ = ["AtmosphereModel", "atmospheric_refraction"]
@@ -45,26 +47,24 @@ class AtmosphereModel(object):
         self.info["res"] = res
 
         hdulist = fits.open(fname)
-        header = hdulist[1].header
         data = hdulist[1].data
         hdulist.close()
 
         ## CHECK: Do these have to go into the top level?
-        self.lam = data["lam"]
-        self.flux = data["flux"]
-        self.dflux = data["dflux1"]
-        self.trans = data["trans"]
-        self.val_unit = u.Unit("ph/s/m2/micron/arcsec2")
+        lam = data["lam"]
+        flux = data["flux"]
+        trans = data["trans"]
+        val_unit = u.Unit("ph/s/m2/micron/arcsec2")
 
-        self.ec = SpectralCurve(lam=self.lam, val=self.flux,
-                                lam_unit=u.um, val_unit=self.val_unit,
-                                Type="Atmospheric emission")
-        self.tc = SpectralCurve(lam=self.lam, val=self.trans,
-                                lam_unit=u.um, val_unit=1.,
-                                Type="Atmospheric transmission")
-                                
-        
-        
+        self.emission = sc.EmissionCurve(lam=lam, val=flux,
+                                         lam_unit=u.um, val_unit=val_unit,
+                                         Type="Atmospheric emission")
+        self.transmission = sc.TransmissionCurve(lam=lam, val=trans,
+                                                 lam_unit=u.um, val_unit=1.,
+                                                 Type="Atmospheric transmission")
+
+
+
     def __repr__(self):
         return "Ich bin ein Atmosphere_Model:\n" + str(self.info)
 
@@ -92,23 +92,23 @@ def atmospheric_refraction(lam, z0=60, temp=0, rel_hum=60, pres=750,
 
     References:
     ==========
-    See Stone 1996 and the review by S. Pedraz - 
-    http://www.caha.es/newsletter/news03b/pedraz/newslet.html 
+    See Stone 1996 and the review by S. Pedraz -
+    http://www.caha.es/newsletter/news03b/pedraz/newslet.html
     """
 
     #### need T, P, RH for Ps, Pw Pa
     T = 273.15 + temp
 
-    Ps = -10474. + 116.43*T - 0.43284*T**2 + 0.0005384*T**3
+    Ps = -10474. + 116.43 * T - 0.43284 * T**2 + 0.0005384 * T**3
     Pw = Ps * rel_hum / 100.
     Pa = pres - Pw
 
     #### need n0 for gamma
     sig = 1. / lam
-    Da = (Pa/T) * (1. + Pa * (57.9E-8 - 0.0009325/T + 0.25844/T**2))
-    Dw = (Pw/T) * (1. + Pw*(1. + 3.7E-4 * Pw) *
-                   (-2.37321E-3 + 2.23366 / T - 710.792 / T**2
-                    + 77514.1 / T**3))
+    Da = (Pa / T) * (1. + Pa * (57.9E-8 - 0.0009325 / T + 0.25844 / T**2))
+    Dw = (Pw / T) * (1. + Pw * (1. + 3.7E-4 * Pw) *
+                     (-2.37321E-3 + 2.23366 / T - 710.792 / T**2
+                     + 77514.1 / T**3))
 
     na = Da * (2371.34 + 683939.7 / (130. - sig**2) + 4547.3 / (38.9 - sig**2))
     nw = Dw * (6487.31 + 58.058 * sig**2 - 0.7115 * sig**4 + 0.08851 * sig**6)
@@ -120,16 +120,16 @@ def atmospheric_refraction(lam, z0=60, temp=0, rel_hum=60, pres=750,
     #k = 1
     k = 1. + 0.005302 * np.sin(lat / 57.29578)**2 \
         - 0.00000583 * np.sin(2. * lat / 57.29578)**2 - 0.000000315 * h
-    
+
     R = k * g * (1 - b) * np.tan(z0 / 57.29578) \
         - k * g * (b - g/2.) * np.tan(z0/57.29578)**3
 
     # the refraction is the side of a triangle, although the triangle
-    # side makes an arc across the sky. 
+    # side makes an arc across the sky.
     # We want the angle that this triangle side is subtending
     # Using the small angle approximation (which is in radians),
     # we can get the angle of refraction
-    
+
     ang = R * 3600 * 57.29578
-    
+
     return ang
