0f456cbe9a7084c4b26b0ced5f0186909185d30f
Kieran Leschinski <kdleschinski@gmail.com>
2016-05-18 10:32:54 +0200

    added docstrings

--- SimCADO/SimCADO/commands.py
+++ SimCADO/SimCADO/commands.py
@@ -1,33 +1,63 @@
-"""commands.py"""
-###############################################################################
-# commands
-#
-# DESCRIPTION
-#
-# commands is essentially a dictionary that holds all the variables that
-# the user may wish to change. It also has some set variable like 'self.pix_res'
-# that can be accessed directly, instead of from the dictionary.
-#
-# Special parameters
-# ==================
-# self.lam_res
-# self.lam
-#
-# self.lam_psf_res
-# self.lam_bin_edges
-# self.lam_bin_centers
-# self.pix_res
-# self.fpa_res
-#
-# self.exptime
-# self.diameter
-# self.area
-# self.verbose
-#
-# TODO list
-# =========
-# - put in a defaults generator
-# - remove the need for the user_commands directory by using internal defaults
+"""
+This module contains classes which control how a simulation is run
+
+
+Summary
+-------
+UserCommands is essentially a dictionary that holds all the variables that
+the user may wish to change. It also has some set variables like `pix_res`
+that can be accessed directly, instead of from the dictionary.
+
+UserCommands is imported directly into the simcado package and is accessable 
+from the main package - `simcado.UserCommands`
+
+If UserCommands is called without any arguments, the default values for MICADO 
+are used. 
+
+
+Routines
+--------
+`UserCommands(filename, default=<path_to_default>)`
+
+
+See Also
+--------
+Class that require a `UserCommands` object directly include:
+- `Detector`
+- `OpticalTrain`
+
+
+Notes
+-----
+
+References
+----------
+
+Examples
+--------
+By default `UserCommands` contains the parameters needed to generate the MICADO
+optical train:
+```
+>>> import simcado
+>>> my_cmds = simcado.UserCommands()
+>>> my_cmds["SCOPE_M1_DIAMETER_OUT"]
+37.3
+```
+
+To list the keywords that are available:
+```
+>>> my_cmds.keys()
+...
+```
+
+The UserCommands object also contains smaller dictionaries for each category of
+keywords - e.g. for the keywords for the instrument:
+```
+>>> my_cmds.inst
+...
+```
+
+"""
 
 
 import os, warnings
@@ -45,35 +75,157 @@ __all__ = ["UserCommands"]
 
 class UserCommands(object):
     """
-    A dictionary class that holds all the keywords from the config files plus
-    a couple of most used variables, i.e. pix_res, lam_bin_edges
+    An extended dictionary with the parameters needed for running a simulation
 
+    
+    Summary
+    -------
+    A `UserCommands` object contains a dictionary which holds all the keywords 
+    from the `default.config` file. It also has attributes which represent the
+    frequently used variables, i.e. `pix_res`, `lam_bin_edges`, `exptime`, etc
+    
+    `<UserCommands>.cmds` is a dictionary that holds all the variables
+    the user may wish to change. It also has some set variables like 
+    `<UserCommands>.pix_res` that can be accessed directly, instead of from the 
+    dictionary.
+
+    `UserCommands` is imported directly into the simcado package and is 
+    accessable from the main package - `simcado.UserCommands`
+
+    If UserCommands is called without any arguments, the default values for 
+    MICADO and the E-ELT are used. 
+
+    
     Parameters
-    ==========
-    - user_filename: path to the user's .config file
-    - master_filename: path to the master.config file
+    ----------
+    filename : str
+        path to the user's .config file
+    default : str, optional
+        path to the default.config file.
+    
+    
+    Attributes
+    ----------
+    Internal dictionaries
+    cmds : dict
+        the dictionary which holds all the keyword-value pairs needed for 
+        running a simualtion
+    obs : dict
+        parameters about the observation
+    sim : dict
+        parameters about the simualtion
+    atmo : dict
+        parameters about the atmosphere
+    scope : dict
+        parameters about the telescope
+    inst : dict
+        parameters about the instrument
+    fpa : dict
+        parameters about the detector array (FPA - Focal Plane Array)
+    hxrg : dict
+        parameters about the chip noise (HxRG - HAWAII 4RG chip series)
+    
+    Attributes pertaining to the purely spectral data sets (e.g. transmission
+    curves, stellar spectra, etc)
+    lam : np.ndarray
+        a vector containing the centres of the wavelength bins used when 
+        resampling the spectra or transmission curves
+    lam_res : float
+        [um] the resolution of the `lam`
+        
+    Attributes pertaining to the binning in spectral space for which different
+    PSFs need to be used
+    lam_psf_res : float
+        [um] the spectal "distance" between layers - i.e. width of the bins
+    lam_bin_edges : array-like
+        [um] the edge of the spectral bin for each layer
+    lam_bin_centers : array-like
+        [um] the centres of the spectral bin for each layer
+    
+    Attributes pertaining to the binning in the spatial plane
+    pix_res : float
+        [arcsec] the internal (oversampled) spatial resolution of the simulation
+    fpa_res : float
+        [arcsec] the field of view of the individual pixels on the detector
+    
+    General attributes
+    verbose : bool
+        Flag for printing intermediate results to the screen (default=True)
+    exptime : float
+        [s] exposure time of a single DIT
+    diameter : float
+        [m] outer diamter of the primary aperture (i.e. M1)
+    area : float
+        [m^2] effective area of the primary aperture (i.e. M1)
+    
+    
+    Methods
+    -------
+    update(new_dict)
+        updates the current `UserCommands` object from another dict-like object
+    keys()
+        returns the keys in the `UserCommands.cmds` dictionary
+    values()
+        returns the values in the `UserCommands.cmds` dictionary
+    
+    Raises
+    ------
+
+    See Also
+    --------
+    Detector, OpticalTrain
+
+    Notes
+    -----
+
+    References
+    ----------
+
+    Examples
+    --------
+    By default `UserCommands` contains the parameters needed to generate the 
+    MICADO optical train:
+    ```
+    >>> import simcado
+    >>> my_cmds = simcado.UserCommands()
+    >>> my_cmds["SCOPE_M1_DIAMETER_OUT"]
+    37.3
+    ```
+    
+    `UserCommands` supports indexing like a dictionary object.
+    ```
+    >>> my_cmds["SCOPE_M1_DIAMETER_OUT"] = 8.2
+    >>> my_cmds["SCOPE_M1_DIAMETER_OUT"]
+    8.2
+    ```
+
+    To list the keywords that are available:
+    ```
+    >>> my_cmds.keys()
+    ...
+    ```
+
+    The `UserCommands` object also contains smaller dictionaries for each category
+    of keywords - e.g. for the keywords describing the instrument:
+    ```
+    >>> my_cmds.inst
+    ...
+    ```
     """
+    
+    
+    def __init__(self, filename=None,
+                 default="../data/default.config"):
 
-    def __init__(self, user_filename=None,
-                 default_filename="../user_commands/default.config"):
-                 #master_filename="../user_commands/master.config"):
-
-        #self.cmds = utils.read_config(master_filename)
-
-        # need to generate a list, because the cmds dict will be updated
-        # fnames = [value for value in self.cmds.values()]
-        # for fname in fnames:
-            # if os.path.exists(fname):
-                # self.cmds.update(utils.read_config(fname))
-            # else:
-                # warnings.warn(fname+" doesn't exist.")
-
-        self.cmds = utils.read_config(default_filename)
+        """
+        Create an extended dictionary of simulation parameters
+        """
+         
+        self.cmds = utils.read_config(default)
 
         if user_filename is not None:
-            self.cmds.update(utils.read_config(user_filename))
+            self.cmds.update(utils.read_config(filename))
 
-        #self.cmds["CONFIG_MASTER"] = master_filename
         self.cmds["CONFIG_USER"]   = user_filename
         self.cmds["CONFIG_DEFAULT"] = default_filename
 
@@ -89,7 +241,7 @@ class UserCommands(object):
         else:
             self.cmds["PSF_MODE"] = "linear_interp"
 
-        # Check if for a filter curve file or a standard broadband name
+        # Check for a filter curve file or a standard broadband name
         if self.cmds["INST_FILTER_TC"] in ["I", "z", "Y", "J", "H", "Ks", "K"]:
             self.cmds["INST_FILTER_TC"] = "../data/TC_filter_" + \
                                             self.cmds["INST_FILTER_TC"] + ".dat"
@@ -99,11 +251,74 @@ class UserCommands(object):
         if self.verbose:
             print("Read in parameters from \n"+"\n".join(fnames))
 
+            
+    def update(self, new_dict):
+        """
+        Update nultiple entries of a `UserCommands` dictionary
+
+
+        Summary
+        -------
+        `update(new_dict)` takes either a normal python `dict` object or a 
+        `UserCommands` object. Only keywords that match those in the 
+        `UserCommands` object will be updated. No warning is given for 
+        misspelled keywords.
+        
+        To update single items in the dictionary, it is recommended to simply
+        call the key and update the value - i.e `<UserCommands>[key] = value`. 
+        
+        Parameters
+        ----------
+        new_dict : dict, `UserCommands`
+
+       
+        Raises
+        ------
+
+        See Also
+        --------
+        UserCommands
+        
+        Notes
+        -----
+
+        Examples
+        --------
+        ```
+        >>> import simcado
+        >>> my_cmds = simcado.UserCommands()
+        >>> new_cmds = {"OBS_EXPTIME" : 60 , "OBS_NDIT" : 10}
+        >>> my_cmds.update(new_cmds)
+        ```
+        """
+
+        if isinstance(new_dict, commands):
+            self.cmds.update(new_dict.cmds)
+        elif isinstance(new_dict, dict):
+            self.cmds.update(new_dict)
+        else:
+            raise ValueError("Cannot update with type: "+type(new_dict))
+        self._update_attributes()
+
+        
+    def keys(self):
+        """
+        Return the keys in the `UserCommands.cmds` dictionary
+        """
+        return self.cmds.keys()
+
+    def values(self):
+        """
+        Return the values in the `UserCommands.cmds` dictionary
+        """
+        return self.cmds.values()
+            
+            
+            
 
     def _update_attributes(self):
         """
-        Update the UserCommand convenience attributes so that they are the same
-        as the dictionary.
+        Update the UserCommand convenience attributes
         """
 
         self.fpa_res    = self.cmds["SIM_DETECTOR_PIX_SCALE"]
@@ -153,8 +368,25 @@ class UserCommands(object):
 
     def _get_lam_bin_edges(self, lam_min, lam_max):
         """
-        Generates lam_bin_edges
+        Generates an array with the bin edges of the layers in spectral space
+        
+        
+        Parameters
+        ----------
+        lam_min, lam_max : float
+            [um] the minimum and maximum wavelengths of the filter range
+        
+        
+        Notes
+        -------
+        Atmospheric diffraction causes blurring in an image. To model this 
+        effect the spectra from a `Source` object are cut into bins based on 
+        how far the photons at different wavelength are diffracted from the 
+        image center. The threshold for defining a new layer based on the how
+        far a certain bin will move is given by `SIM_ADC_SHIFT_THRESHOLD`. The
+        default value is 1 pixel.
         """
+        
         effectiveness = self.cmds["INST_ADC_PERFORMANCE"] / 100.
         if effectiveness == 1.:
             lam_bin_edges = np.array([lam_min, lam_max])
@@ -189,6 +421,9 @@ class UserCommands(object):
 
 
     def _split_categories(self):
+        """
+        Generate smaller category-specific dictionaries
+        """
         self.obs    = {i:self.cmds[i] for i in self.cmds.keys() if "OBS" in i}
         self.sim    = {i:self.cmds[i] for i in self.cmds.keys() if "SIM" in i}
         self.atmo   = {i:self.cmds[i] for i in self.cmds.keys() if "ATMO" in i}
@@ -197,21 +432,7 @@ class UserCommands(object):
         self.fpa    = {i:self.cmds[i] for i in self.cmds.keys() if "FPA" in i}
         self.hxrg   = {i:self.cmds[i] for i in self.cmds.keys() if "HXRG" in i}
 
-    def update(self, x_dict):
-        if isinstance(x_dict, commands):
-            self.cmds.update(x_dict.cmds)
-        elif isinstance(x_dict, dict):
-            self.cmds.update(x_dict)
-        else:
-            raise ValueError("Cannot update with type: "+type(x_dict))
-        self._update_attributes()
-
-    def keys(self):
-        return self.cmds.keys()
-
-    def values(self):
-        return self.cmds.values()
-
+   
     def __str__(self):
         if self.cmds["CONFIG_USER"] is not None:
             return "A dictionary of commands compiled from " + \
@@ -233,5 +454,6 @@ class UserCommands(object):
     ### Add to the update that all the cmds.variable are updated when
     ### the dicts are updated
 
-class bloedsinn():
+    
+class __bloedsinn():
     pass
