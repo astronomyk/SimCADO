b1a6f8227c8debb1bd101bfd199e3e68e9373798
Kieran Leschinski <kdleschinski@gmail.com>
2016-06-09 11:59:22 +0200

    pacakge gen stuff

--- SimCADO/create.bat
+++ SimCADO/create.bat
@@ -2,7 +2,7 @@ del .\dist\*.zip
 dir .\dist\
 python setup.py sdist
 pip uninstall simcado
-pip install -I .\dist\simcado-0.1dev.zip
+pip install -I .\dist\simcado-0.2dev.zip
 
 @echo off
 echo apple is not the hammer 
