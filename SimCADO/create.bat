3de2a95ab34c859eed681f0adcdc1c5118e15069
Kieran Leschinski <kdleschinski@gmail.com>
2016-06-10 09:14:53 +0200

    new build - minor changes

--- SimCADO/create.bat
+++ SimCADO/create.bat
@@ -1,8 +1,8 @@
 del .\dist\*.zip
 dir .\dist\
 python setup.py sdist
-pip uninstall simcado
-pip install -I .\dist\simcado-0.2dev.zip
+:: pip uninstall simcado
+:: pip install -I .\dist\simcado-0.2dev.zip
 
 @echo off
 echo apple is not the hammer 
