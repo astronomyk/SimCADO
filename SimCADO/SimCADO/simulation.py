7d1bd9fcb018b99eafcc7ee2b64b2a46a95b4b05
Kieran Leschinski <kdleschinski@gmail.com>
2016-06-25 21:39:16 +0200

    rewritten

--- SimCADO/SimCADO/simulation.py
+++ SimCADO/SimCADO/simulation.py
@@ -1,144 +1,39 @@
-###############################################################################
-# SimulationRun
-#
-# DESCIPTION
-#
-# File Structure needed to run the simulation
-#
-#
-#
-# Need 2 different commands dictionaries as input
-#  - Observation parameters (Alt, Az, Exptime, etc)
-#  - Observatory parameters (area, instrument configuration, psfs, etc)
-#  -
-#
-#
-# Classes:
-#
-#
-# Methods:
-#
-#
-# 1. Read in commands
-#    Create commands objects for: optical train and observing run.
-#    The observing run will be used for science, atmo and mirror photons.
-#
-# 2. Create optics,
-#    which in turn creates all the individual objects
-#
-# 3. Create sources.
-#    This in turn means creating a source for every source of background
-#    photons, e.g. for the atmosphere and every mirror.
-#    If the cube is too large (>1GB), the source for the science case will
-#    need to be split into x lists (based on integrated pixel brightness) and
-#    the simulation run x times with the same optics.
-#
-# 4. To all the sources do the following:
-#    - Apply spectral effects (Transmission curves)
-#    - Apply plane effects (distortion, rotation), where applicable
-#    - Apply 3D PSF cube effects (Telescope PSF, ADC PSF), where applicable
-#    - Collapse the cube into a 2D image
-#    - Apply 2D PSF effects
-#
-# 5. Convert image to electrons and add the detector effects
+"""
+simualtion.py
+"""
 
+from astropy.io import ascii
+import simcado as sim
 
-
-######## Taken out of my IPython Notebook - not to be take seriously
-
-import sys, os
-import warnings
-
-import numpy as np
-
-from astropy.io import fits
-import astropy.units as u
-
-try:
-    import simcado.psf as psf
-    import simcado.spectral as sc
-    import simcado.source as lo
-    import simcado.commands as uc
-    import simcado.optics as ot
-    import simcado.utils as utils
-except:
-    import psf as psf
-    import spectral as sc
-    import source as lo
-    import commands as uc
-    import optics as ot
-    import utils
-
-class Simulation(object):
+def run(src, cmds=None, opt_train=None, fpa=None, 
+        small_fov=True, filename=None, return_internals=False):
     """
+    Run a MICADO simulation with default parameters
     """
-
-    def __init__(self, cmds=None, filename=None):
-        """
-        """
-        #self.set_user_commands(filename)
-        #if  cmds  is not None: self.cmds = cmds
-
-        self.cmds = uc.commands()
-        self.opt = ot.optics(self.cmds)
-        self.src = lo.Source(self.cmds["OBS_INPUT_NAME"])
-        self.obj = lo.source(self.src, self.cmds)
-        self.obj.apply_optical_train(opt)
-        #self.raw_image = self.opt.read_detector(self.obj, self.cmds)
+    
+    if cmds is None: 
+        cmds = commands.UserCommands()
+        cmds["INST_FILTER_TC"] = "J"
+    
+    if small_fov:
+        chip_layout = """#  id    x_cen    y_cen   x_len   y_len
+                         #       arcsec   arcsec   pixel   pixel
+                             0        0        0    1024    1024"""
+        cmds["FPA_CHIP_LAYOUT"] = chip_layout
         
-        hdu = fits.PrimaryHDU(self.obj.array)
-        hdu.header["BUNIT"] = "ph/s"
-        hdu.header["CDELT1"] = opt.pix_res, "[arcsec]"
-        hdu.header["CDELT2"] = opt.pix_res, "[arcsec]"
-        hdu.header["AREA"] = opt.cmds.area, "M1 area [m2]"
-        
-        if filename is not None:
-            try: 
-                hdu.writeto(filename, clobber=True)
-            except:
-                print("Unable to save to "+filename)
-        else: return hdu
-        
-
-    def set_user_commands(self, user_filename=None):
-        """
-        - get master.config
-        - read in all the other configs and add them to cmds
-        - read in the file with user overrides
-
-        Optional keywords:
-        user_filename: path to the user.config file
-        """
-        self.cmds = utils.read_config("../user_commands/master.config")
-        fnames = [self.cmds[key] for key in self.cmds.keys()]
-        for fname in fnames:
-            if os.path.exists(fname):
-                self.cmds.update(utils.read_config(fname))
-            else:
-                warnings.warn(fname+" doesn't exist")
-
-        if user_filename is not None:
-            self.cmds.update(utils.read_config(user_filename))
-
-
-    def read_optical_train(self, filename):
-        # if we should use a previous optical system, import is
-        pass
-
-    def save_optical_train(self, filename, clobber=False):
-        # save the current optical system to a FITS file
-        pass
-
-    def make_optical_train(self):
-        # if we should use a previous optical system, import it
-        # else generate a new one
-        pass
-
-    def read_source(self, filename):
-        pass
-
-    def run():
-        pass
-
-
-
+    if opt_train is None:
+        opt_train = optics.OpticalTrain(cmds)
+    if fpa is None:
+        fpa = detector.Detector(cmds)
+     
+    print(fpa.layout)
+    src.apply_optical_train(opt_train, fpa)
+    
+    if filename is not None:
+        fpa.read_out(filename=filename, to_disk=True)
+    else: 
+        hdu = fpa.read_out()
+        if return_internals:
+            return hdu, (cmds, opt_train, fpa)
+        else:
+            return hdu
