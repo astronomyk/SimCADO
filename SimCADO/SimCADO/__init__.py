25e11bf0bd30f093ddfb18a892a1f3e302565136
Kieran Leschinski <kdleschinski@gmail.com>
2016-06-25 21:37:08 +0200

    fiddled with the order

--- SimCADO/SimCADO/__init__.py
+++ SimCADO/SimCADO/__init__.py
@@ -4,17 +4,17 @@ End-to-end simulator for MICADO on the E-ELT
 """
 # Import all the modules to go under simcado.detector
 from . import utils
-from . import optics_utils
 
 from . import spectral
 from . import spatial
 from . import psf
 
+from . import detector
 from . import optics  
 from . import commands
-from . import detector
 from . import source  
 
+from . import optics_utils
 from . import defaults
 
 from .version import version as __version__
@@ -28,6 +28,5 @@ from .commands  import UserCommands
 
 # don't import these ones just yet
 #from .SpectralGrating  import * 
-#from .simulation       import *
-
+from . import simulation
 
