6d7c3a74a85a4a536984867e01d2d39fe8c513d4
Kieran Leschinski <kdleschinski@gmail.com>
2016-07-21 11:28:21 +0200

    added snr() added kwargs to run()

--- SimCADO/SimCADO/simulation.py
+++ SimCADO/SimCADO/simulation.py
@@ -6,25 +6,25 @@ from astropy.io import ascii
 import simcado as sim
 
 def run(src, cmds=None, opt_train=None, fpa=None, 
-        small_fov=True, filename=None, return_internals=False):
+        micado_fpa=False, filename=None, return_internals=False,
+        **kwargs):
     """
     Run a MICADO simulation with default parameters
     """
     
+
     if cmds is None: 
-        cmds = commands.UserCommands()
-        cmds["INST_FILTER_TC"] = "J"
+        cmds = sim.UserCommands()
+        cmds["INST_FILTER_TC"] = "K"
+    cmds.update(kwargs)
     
-    if small_fov:
-        chip_layout = """#  id    x_cen    y_cen   x_len   y_len
-                         #       arcsec   arcsec   pixel   pixel
-                             0        0        0    1024    1024"""
-        cmds["FPA_CHIP_LAYOUT"] = chip_layout
+    if micado_fpa:
+        cmds["FPA_CHIP_LAYOUT"] = "default"
         
     if opt_train is None:
-        opt_train = optics.OpticalTrain(cmds)
+        opt_train = sim.OpticalTrain(cmds)
     if fpa is None:
-        fpa = detector.Detector(cmds)
+        fpa = sim.Detector(cmds)
      
     print(fpa.layout)
     src.apply_optical_train(opt_train, fpa)
@@ -37,3 +37,66 @@ def run(src, cmds=None, opt_train=None, fpa=None,
             return hdu, (cmds, opt_train, fpa)
         else:
             return hdu
+            
+            
+def snr(mags, filter="K", exptime=18000, ndit=1, cmds=None):
+    """
+    Return the signal-to-noise for a list of magnitudes in a specific filter
+    
+    Uses the standard setup for MICADO and calculated the signal to noise
+    for a list of magnitudes in `mags` in a certain broadband `filter`. A 
+    custom UserCommands object can also be used. Note that this runs a basic 
+    SimCADO simulation len(mags) times, so execution time can be many minutes. 
+    
+    Parameters
+    ----------
+    mags : array-like
+        [vega mags] The magnitude(s) of the source(s)
+    filter : str, optional
+        Default is "K". Acceptable broadband filters are UBVRIzYJHKKs
+    exptime : float
+        [s] Total exposure time length. Default is 18000s (5 hours)
+    ndit : int, optional
+        Number of readouts during the period `exptime`. Default is 1
+    cmds : simcado.UserCommands, optional
+        A custom set of commands for the simulations. If not specified, SimCADO
+        uses the default MICADO parameters
+    
+    Returns
+    -------
+    sn : np.ndarray
+        An array of signal-to-noise ratios for the magnitudes given
+    
+    """
+    import numpy as np
+    import simcado
+    
+    cmd = simcado.UserCommands()
+    cmd["OBS_EXPTIME"] = exptime / ndit
+    cmd["OBS_NDIT"] = ndit
+    cmd["INST_FILTER_TC"] = filter
+    
+    opt = simcado.OpticalTrain(cmd)
+    
+    sn = []
+    for mag in mags:
+        src = simcado.source.star(mag)
+
+        fpa = simcado.Detector(cmd)
+        src.apply_optical_train(opt, fpa, chips=0)
+        hdu = fpa.read_out()
+
+        im = hdu[0].data
+        cx, cy = np.array(im.shape) // 2 
+        n = 5
+        sig = np.sum(im[cx-n:cx+n+1, cy-n:cy+n+1])
+        av = np.average(im[:200, :50])
+        std = np.std(im[:200, :50])
+
+        n_pix = (2*n+1)**2
+        only_sig = sig - av*n_pix
+        only_noise = av# * np.sqrt(n_pix)
+
+        sn += [only_sig/only_noise]
+    
+    return np.array(sn)
