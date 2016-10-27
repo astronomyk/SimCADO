e24d38ace08bc70bd5b8286300b7b08dd873aa8b
gastronomyk <kdleschinski@gmail.com>
2016-10-27 16:20:58 +0200

    updated path names

--- SimCADO/create_docs.bat
+++ SimCADO/create_docs.bat
@@ -5,11 +5,10 @@ cd site/API/
 mkdir _source
 mkdir _build
 
-cd _source
-sphinx-apidoc -f -F -o .\ simcado
+cd ../../../..
+sphinx-apidoc -f -F -o simcado/docs/site/API/_source simcado
+
+cd simcado/docs/site/API/_source
 copy ..\..\..\conf.py conf.py
-
-sphinx-build -b html .\ ..\_build
-
-cd ..\..\..\..\..
+sphinx-build -b html -E .\ ..\_build
 pause
