0dc97e1e2ccba025300d9d7f2981a213e71fbdc3
gastronomyk <kdleschinski@gmail.com>
2016-11-03 15:19:02 +0100

    added SimCADO_defaults.py

--- SimCADO/create_docs.bat
+++ SimCADO/create_docs.bat
@@ -2,6 +2,8 @@ D:
 cd D:\Dropbox\Uni\PhD\SimCADO\SimCADO
 
 cd simcado/docs/
+
+python SimCADO_defaults.py
 mkdocs build --clean
 
 cd site/API/
@@ -14,3 +16,5 @@ sphinx-apidoc -f -F -o simcado/docs/site/API/_source simcado
 cd simcado/docs/site/API/_source
 copy ..\..\..\conf.py conf.py
 sphinx-build -b html -E .\ ..\_build
+
+pause
