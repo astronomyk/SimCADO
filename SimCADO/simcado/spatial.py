5ba0fa24cba10a6bb288768d1432a2859cf8d153
gastronomyk <kdleschinski@gmail.com>
2016-11-16 14:23:22 +0100

    minutia

--- SimCADO/simcado/spatial.py
+++ SimCADO/simcado/spatial.py
@@ -101,7 +101,7 @@ def _line_blur(arr, shift, kernel="gaussian", angle=0):
 
     tmp_arr = np.zeros(arr.shape)
     for x, y, w in zip(dx, dy, weight):  # TODO: x, y unused? (OC)
-        tmp_arr += spi.shift(arr, (dx, dy), order=1) * w
+        tmp_arr += spi.shift(arr, (x, y), order=1) * w
 
     return tmp_arr
 
@@ -165,7 +165,11 @@ def derotator(arr, cmds):
         eff = 1. - (cmds["INST_DEROT_PERFORMANCE"] / 100.)
         kernel = cmds["INST_DEROT_PROFILE"]
         angle = eff * cmds["OBS_EXPTIME"] * 15 / 3600.
-
+        
+        edge_smear = angle / cmds.pix_res
+        if edge_smear > 50:
+            print("The smear at the detector edge is large:", edge_smear, "[px]")
+        
         return _rotate_blur(arr, angle, kernel=kernel)
     else:
         return arr
@@ -204,7 +208,7 @@ def adc_shift(cmds):
 
     ## convert angle shift into number of pixels
     ## pixel shifts are defined with respect to last slice
-    rel_shift = (angle_shift - angle_shift[-1]) / cmds.pix_res
+    rel_shift = (angle_shift - angle_shift[-1])
     if np.max(np.abs(rel_shift)) > 1000:
         raise ValueError("Pixel shifts too great (>1000), check units")
 
@@ -212,4 +216,5 @@ def adc_shift(cmds):
     x = -rel_shift * np.sin(np.deg2rad(para_angle)) * (1. - effectiveness)
     y = -rel_shift * np.cos(np.deg2rad(para_angle)) * (1. - effectiveness)
 
+    ## return values are in [arcsec]
     return x, y
