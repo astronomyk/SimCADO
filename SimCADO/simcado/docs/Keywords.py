c84becfe3dd179a7eb9caa105118972a0cb7ed19
gastronomyk <kdleschinski@gmail.com>
2017-02-07 10:41:47 +0100

    outputs reST now, instead of md

--- SimCADO/simcado/docs/Keywords.py
+++ SimCADO/simcado/docs/Keywords.py
@@ -5,7 +5,8 @@ A code snippet to generate the Keywords document
 import os
 
 text = """
-# List of Keywords for Controlling SimCADO
+Keywords for Controlling SimCADO
+================================
 """
 
 f = open("../data/default.config", "r")
@@ -16,21 +17,27 @@ i=0
 while i < len(data)-1:
     if "#####" in data[i]:
         i += 1
-        text += "#" + data[i]
+        d = data[i][2:]
+        text += d
+        text += "-" * len(d)
         text += """
-```
-Keyword                 Default     [units] Explanation
------------------------------------------------------------------------------------------------
+
+.. code-block:: none
+
+    Keyword                 Default     [units] Explanation
+    -----------------------------------------------------------------------------------------------
 """
         i += 1
-    elif data[i] == "\n" and data[i+1] == "\n":
-        text += "```\n"
-        i += 1
+#    elif data[i] == "\n" and data[i+1] == "\n":
+#        text += "\n"
+#        i += 1
     else:
-        text += data[i]
+        text += "    " + data[i]
         i += 1
     
+
+text += "\n"
     
-f = open("./source/Keywords.md", "w")
+f = open("./source/Keywords.rst", "w")
 f.write(text) 
 f.close()
