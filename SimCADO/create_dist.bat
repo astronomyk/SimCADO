37aed752d8f41802ee52d3b9891ff665770724f0
gastronomyk <kdleschinski@gmail.com>
2017-03-13 10:00:02 +0100

    just create the distribution, forget the docs

---
+++ SimCADO/create_dist.bat
@@ -0,0 +1,19 @@
+D:
+cd D:\Dropbox\Uni\PhD\SimCADO\SimCADO
+
+del .\dist\*.tar.gz
+del .\dist\*.zip
+dir .\dist\
+python setup.py sdist
+
+@echo off
+
+setlocal enabledelayedexpansion
+set params=
+for /f "delims=" %%a in ('dir .\dist\* /s/b') do set params=!params! %%a 
+echo !params!
+
+pip install -I !params!
+
+echo apple is not the hammer 
+pause
