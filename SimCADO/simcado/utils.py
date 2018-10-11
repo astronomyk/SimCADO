335579ae3441d9c21329f2905ae9d3f2e91fcea6
oczoske <ollicz68@yahoo.com>
2018-10-11 16:57:32 +0200

    Updated utils.py

--- SimCADO/simcado/utils.py
+++ SimCADO/simcado/utils.py
@@ -39,6 +39,7 @@ __pkg_dir__ = os.path.dirname(inspect.getfile(inspect.currentframe()))
 #__all__ = ["unify", "parallactic_angle", "poissonify",
 #           "atmospheric_refraction", "nearest", "add_keyword"]
 
+
 def msg(cmds, message, level=3):
     """
     Prints a message based on the level of verbosity given in cmds
@@ -601,7 +602,6 @@ def bug_report():
     packages = ["simcado", "astropy", "numpy", "scipy", "poppy", "wget"]
 
     # Check Python version
-    import sys
     print("Python:\n", sys.version)
     print("")
 
@@ -621,3 +621,47 @@ def bug_report():
     print("         Release: ", osinfo.release)
     print("         Version: ", osinfo.version)
     print("         Machine: ", osinfo.machine)
+
+
+def find_file(filename, path=None, silent=False):
+    '''Find a file in search path
+
+    Parameters
+    ----------
+    filename : str
+        name of a file to look for
+    path : list
+        list of directories to search (default: ['./'])
+    silent : bool
+        if True, remain silent when file is not found
+
+    Returns
+    -------
+    Absolute path of the file
+    '''
+    import simmetis as sim
+
+    if path is None:
+        path = sim.__search_path__
+
+    if os.path.isabs(filename):
+        # absolute path: only path to try
+        trynames = [filename]
+    else:
+        # try to find the file in a search path
+        trynames = [os.path.join(trydir, filename)
+                    for trydir in path]
+
+    for fname in trynames:
+        if os.path.exists(fname):   # success
+            # strip leading ./
+            while fname[:2] == './':
+                fname = fname[2:]
+            return fname
+        else:
+            continue
+
+    # no file found
+    if not silent:
+        print("File cannot be found: " + filename)
+    return None
