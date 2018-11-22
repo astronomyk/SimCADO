3ab2ac57211f319a1f79e452b3559438cf0c18e4
Kieran <kdleschinski@gmail.com>
2018-11-22 21:43:30 +0100

    rtd29

--- SimCADO/simcado/docs/source/conf.py
+++ SimCADO/simcado/docs/source/conf.py
@@ -53,7 +53,7 @@ extensions = [
 
 apidoc_module_dir = '../../..'
 apidoc_output_dir = 'reference'
-apidoc_excluded_paths = ['/tests/', '../../../setup.py', '../../version.py']
+apidoc_excluded_paths = ['simcado/tests/', '../../../setup.py', '../../version.py']
 apidoc_separate_modules = True
 
 print(os.getcwd())
