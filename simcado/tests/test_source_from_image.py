6cc4f460cdd4b372c6a30fec20f066a2783a8656
miguelverdugo <mverduol@gmail.com>
2019-02-08 15:05:43 +0100

    dowload the files first?

--- simcado/tests/test_source_from_image.py
+++ simcado/tests/test_source_from_image.py
@@ -23,6 +23,7 @@ def mock_dir():
 
 MOCK_DIR = mock_dir()
 
+sim.get_extras()
 cmds = sim.UserCommands(os.path.join(MOCK_DIR, "default.conf"))
 cmds["SIM_DATA_DIR"] = __data_dir__
 print(cmds["SIM_DATA_DIR"])
