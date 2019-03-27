c9663eb4d53a686096cbdd9f0ded1213844def78
Kieran <kdleschinski@gmail.com>
2019-03-27 13:16:10 +0100

    basic test for finding PSF. Expected to fail on travis

--- simcado/tests/test_basic_integration_tests.py
+++ simcado/tests/test_basic_integration_tests.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 from astropy.io import fits
 import simcado as sim
@@ -9,8 +10,13 @@ import simcado as sim
 #         hdu = sim.run(src, OBS_EXPTIME=3600, SCOPE_PSF_FILE="PSF_MCAO.fits",
 #                       filter_name="J", mode="zoom")
 #         assert isinstance(hdu, fits.HDUList)
-#
-#
 
 
-class Test
+class TestTravisDownloadsGetExtras:
+    def test_find_psf_file_with_find_file(self):
+        dirpath = "C:\Work\irdb\_Legacy_packages\MICADO"
+        filename = "PSF_SCAO.fits"
+        filepath = sim.utils.find_file(filename, path=[dirpath])
+        print(filepath, os.path.join(dirpath, filename))
+
+        assert os.path.exists(filepath) is not None
